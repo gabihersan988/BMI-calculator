7183a2de86c7f727dd1bacded2af8416eec24280
Oscar Miras <oscar.miras@gmail.com>
2022-01-12 11:47:04 +0100

    act

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const form = document.querySelector('form');
 
 form.addEventListener('submit', function (e) {
+
+  // Para que sirve 'preventDefault'? Intenta averiguarlo
   e.preventDefault();
 
   console.log("Valor del input: ", e.target.value)
